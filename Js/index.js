4a1f99b566a379849f88da4436378f80640ca39a
Sasha-Villegas <sashavillegas30@gmail.com>
2022-08-05 01:29:36 -0300

    API´s y Fetch

--- Js/index.js
+++ Js/index.js
@@ -89,3 +89,52 @@ function loginform(e){
 
 loginnone.classList.toggle("d-none")
 }
+
+
+
+
+// TOASTIFY BASE 
+
+function toastError(text){
+    Toastify({
+       text: text,
+       duration: "2500",
+       gravity: 'bottom',
+       style: {
+          background: "red",
+          boxShadow: 'none',
+          maxWidth: '70%'
+       },
+    }).showToast();
+}
+
+function toastOK(text,duration,destination){
+   Toastify({
+      text: text,
+      duration: duration,
+      gravity: 'bottom',
+      close: true,
+      destination: destination,
+      style: {
+         background: "#00a753",
+         boxShadow: 'none',
+         maxWidth: '70%'
+      },
+      onClick: ()=>{
+
+      }
+   }).showToast();
+}
+
+//Estilo Click en Botones
+function estiloBtnClick(){
+    const botones = document.getElementsByTagName('button');
+    for (const btn of Array.from(botones)) {
+        btn.addEventListener("click", ()=>{
+            btn.style.cssText = 'background-color: #2bf8bb; color: black; border-color: #24584d;';
+            setTimeout(function(){
+                btn.removeAttribute('style');          
+                }, 150);
+        })
+        }
+    }
